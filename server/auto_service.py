9d9dbca5c1d51e18e78b57bfd945181c3d1fa89f
xuanbaojr <phanxuanbaoa0@gmail.com>
2025-03-31 22:30:43 +0700

    step 0 test grpc connection

---
+++ server/auto_service.py
@@ -0,0 +1,11 @@
+import cv2
+import os
+import uuid
+
+current_dir = os.path.dirname(__file__)
+
+class AutoService:
+    def __init__(self):
+        pass
+    def get_instruction(self, frame1, frame2, frame3, frame4):
+        return "ok"
