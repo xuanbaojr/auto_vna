5d70df8d1cee9be182fe982919f243cf85c9b0e6
xuanbaojr <phanxuanbaoa0@gmail.com>
2025-04-01 15:46:08 +0700

    update step10

--- client/client_grpc.py
+++ client/client_grpc.py
@@ -3,10 +3,12 @@ import instruction_pb2
 import instruction_pb2_grpc
 import cv2
 import numpy as np
+import time
 class CameraClient:
     def __init__(self):
         channel = grpc.insecure_channel("localhost:50051")
         self.stub = instruction_pb2_grpc.GRPCServiceStub(channel)
+        self.session_id = None
         
     def call_grpc_stream(self, frame1, frame2, frame3, frame4, type_instruction):
         """Send frames to gRPC server and get instruction stream"""
@@ -27,10 +29,34 @@ class CameraClient:
 
 if __name__ == "__main__":
     client = CameraClient()
-    frame1 = cv2.imread("im_.png")
-    frame2 = cv2.imread("im_.png")
-    frame3 = cv2.imread("im_.png")
-    frame4 = cv2.imread("im_.png")
-    type_instruction = "is_check"
-    response = client.call_grpc_stream(frame1, frame2, frame3, frame4, type_instruction)
-    print(response.instruction_str)
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Error: Could not open webcam")
+        exit()
+    try:
+        while True:
+            # Capture frame-by-frame
+            ret, frame1 = cap.read()
+            frame2 = frame1.copy()
+            frame3 = frame1.copy()
+            frame4 = frame1.copy()
+            if ret:
+                if client.session_id is None:
+                    type_instruction = "clothes"
+                    try:
+                        response = client.call_grpc_stream(
+                            frame1, frame2, frame3, frame4,
+                            type_instruction
+                        )
+                        print(response.instruction_str)
+                    except Exception as e:
+                        print(f"Error calling gRPC: {e}")
+                
+            cv2.imshow('Real-time Processing', frame1)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
